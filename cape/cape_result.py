ea2779351a145aa9453be4fd6727c84555b1aac5
cccs-mog <mlguilb@mlguilb-dev-ubuntu.53wq0amrcsbuxnt44apgcucn4h.ux.internal.cloudapp.net>
2024-05-02 18:56:37 +0000

    Adding the lookup_type to the OID and adding some more API

--- cape/cape_result.py
+++ cape/cape_result.py
@@ -95,7 +95,9 @@ CONNECT_API_CALLS = [
     "InternetConnectA",
     "WSAConnect",
     "InternetOpenUrlA",
+    "InternetOpenUrlW",
     "WinHttpConnect",
+    "WSAConnectByNameW",
 ]
 DNS_API_CALLS = [
     "getaddrinfo",
@@ -103,6 +105,9 @@ DNS_API_CALLS = [
     "InternetConnectA",
     "GetAddrInfoW",
     "gethostbyname",
+    "DnsQuery_A",
+    "DnsQuery_UTF8",
+    "DnsQuery_W",
 ]
 HTTP_API_CALLS = [
     "send",
@@ -112,8 +117,10 @@ HTTP_API_CALLS = [
     "InternetCrackUrlA",
     "InternetCrackUrlW",
     "InternetOpenUrlA",
+    "InternetOpenUrlW",
     "WinHttpConnect",
     "WSASend",
+    "URLDownloadToCacheFileW",
 ]
 BUFFER_API_CALLS = [
     "send",
@@ -1045,6 +1052,7 @@ def process_network(
                     "transport_layer_protocol": transport_layer_protocol,
                     "connection_type": NetworkConnection.DNS,
                     "dns_details": {"domain": request},
+                    "lookup_type": attempt.get("type")
                 }
             )
             objectid = ontres.create_objectid(
@@ -1472,14 +1480,14 @@ def _get_dns_map(
                 (network_call[api_call] for api_call in DNS_API_CALLS if api_call in network_call),
                 {},
             )
-            if dns != {} and (dns.get("hostname") or dns.get("servername")):
+            if dns != {} and (dns.get("hostname") or dns.get("servername") or dns.get("nodename")):
                 for request, attempts in dns_requests.items():
                     for index, attempt in enumerate(attempts):
                         answers = attempt["answers"]
                         if answers == None:
                             continue
                         for answer in answers:
-                            if not answer.isdigit() in [dns.get("hostname"), dns.get("servername")]:
+                            if not answer.isdigit() in [dns.get("hostname"), dns.get("servername", dns.get("nodename"))]:
                                 if not dns_requests[request][index].get("process_name"):
                                     dns_requests[request][index]["process_name"] = process_details["name"]
 
