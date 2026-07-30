589d211ca66ee85115bbba4d397af748c084fa32
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2026-07-30 12:13:41 -0400

    Update test_cape.py

--- tests/test_cape.py
+++ tests/test_cape.py
@@ -2195,7 +2195,7 @@ class TestCapeMain:
         correct_artifact_list.append(
             {
                 "path": f"{cape_class_instance.working_directory}/{task_id}/CAPE/yarahit.exe",
-                "name": f"{task_id}_3_yarahit.exe",
+                "name": f"dump_{task_id}_3_yarahit.exe",
                 "description": "Memory Dump",
                 "to_be_extracted": True,
             }
