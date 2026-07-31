68f65bbf4a857fd5454ed93aa0c911eca9e07f9a
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2026-07-31 11:57:43 -0400

    Update test_signatures.py

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -3,14 +3,14 @@ import pytest
 
 class TestSignatures:
     @staticmethod
-    @pytest.mark.parametrize("sig, correct_int", [("blah", "unknown"), ("NetworkCnCHTTPSGeneric", "Network")])
+    @pytest.mark.parametrize("sig, correct_int", [("blah", "unknown"), ("NetworkCnCHTTPSGeneric", "network")])
     def test_get_category(sig, correct_int):
         from cape.signatures import get_category
 
         assert get_category(sig) == correct_int
 
     @staticmethod
-    @pytest.mark.parametrize("sig, correct_string", [("blah", "unknown"), ("NetworkCnCHTTPSGeneric", "Network")])
+    @pytest.mark.parametrize("sig, correct_string", [("blah", "unknown"), ("NetworkCnCHTTPSGeneric", "network")])
     def test_get_signature_category(sig, correct_string):
         from cape.signatures import get_signature_category
 
